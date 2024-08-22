29b598de286127d78c90fecd93b64a2a74d9aeeb
AppleNote <a7201492425@gmail.com>
2024-08-22 10:50:55 +0900

    이벤트 처리

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import ConditionalRender from "./component_manage/ConditionalRender";
 import Example2 from "./component_manage/example/Example2";
 import Example2prototype from "./component_manage/example/Example2prototype";
 import ListRender from "./component_manage/ListRender";
+import EventComponent from "./interaction/EventComponent";
 
 function App() {
     return (
@@ -21,9 +22,10 @@ function App() {
             {/* <Gallery /> */}
             {/* <ConditionalRender /> */}
             {/* <Gallery /> */}
-            <Example2 />
+            {/* <Example2 /> */}
             {/* <Example2prototype /> */}
             {/* <ListRender /> */}
+            <EventComponent />
         </>
     );
 }
