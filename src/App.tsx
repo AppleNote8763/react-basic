1f8b41337af0bea97fb0d13b737a0855f35d4fc9
AppleNote <a7201492425@gmail.com>
2024-09-03 10:07:05 +0900

    2024.09.03. 10:07

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import QueryString from "./router/QueryString";
 import PathVariable from "./router/PathVariable";
 import PathMove from "./router/PathMove";
 import Zustand from "./zustand/Zustand";
+import ReactCookie from "./react_cookie/ReactCookie";
 
 // react-router 패키지:
 // - react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라이브러리
@@ -83,6 +84,7 @@ function App() {
             </Route>
 
             <Route path="/zustnad" element={<Zustand />} />
+            <Route path="/react-cookie" element={<ReactCookie />} />
 
             <Route path="*" element={<h1>404!!!</h1>} />
 
